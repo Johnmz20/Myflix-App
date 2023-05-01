2218e721b4ad65ac6ab2187b80c8e00be5322ae7
john meraz <102620932+Johnmz20@users.noreply.github.com>
2023-05-01 18:08:34 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,12 +1,14 @@
-const jwtSecret = 'your_jwt_secret'; 
+const jwtSecret = 'your_jwt_secret';
+
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
 
 require('./passport'); 
 
+
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Username, 
+    subject: user.Username,
     expiresIn: '7d', 
     algorithm: 'HS256' 
   });

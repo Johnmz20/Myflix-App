4ab26eb63d26d581fa9ab9f647c2876cd9230b8d
Johnmz20 <John.Meraz22@qmail.cuny.edu>
2023-08-14 15:53:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require("cors");
-let allowedOrigins = ["http://localhost:3000", "http://testsite.com","http://localhost:1234","https://jm-myflix.netlify.app/login"];
+let allowedOrigins = ["http://localhost:8015", "http://testsite.com","http://localhost:3000","https://jm-myflix.netlify.app/login"];
 app.use(
     cors({
         origin: (origin, callback) => {
